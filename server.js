4d2e1b4f945e1b26631f62213e6dafd32dbf3886
Andrés Felipe Galeano Espósito <felipegcofdi@gmail.com>
2022-11-08 07:50:01 -0500

    Init deploy

---
+++ server.js
@@ -0,0 +1,27 @@
+const { json } = require('body-parser');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+//   res.send('Hello from App Engine!');
+  res.send(JSON.stringify( {
+    "product_id": "SKU_12345",
+    "product_name": "Really comfortable shoes",
+    "product_brand": "Adsmurai",
+    "categories": {
+      "product_category": "Apparel",
+      "product_category2": "Adult",
+      "product_category3": "Shoes"
+    },
+    "prices": {
+      "product_price": "39,99",
+      "product_currency": "€"
+    }
+  }));
+});
+
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
